b4b3c457dff915a49ffe925b52129276044f78a2
dasom <ds92ko@gmail.com>
2025-12-24 07:22:09 +0900

    feat: API 호출 최적화 (캐싱 및 Promise.all 병렬 처리)
    - API 캐싱 유틸리티 추가로 중복 요청 방지
    - Promise.all 올바르게 사용하여 병렬 실행
    - 다이얼로그 열릴 때만 데이터 로드 (지연 로딩)

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -28,8 +28,8 @@ import {
   VStack,
   Wrap,
 } from '@chakra-ui/react';
-import axios from 'axios';
 import { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { cachedFetch } from './apiCache.ts';
 import { DAY_LABELS } from './constants.ts';
 import { useScheduleContext } from './ScheduleContext.tsx';
 import { Lecture } from './types.ts';
@@ -82,8 +82,8 @@ const TIME_SLOTS = [
 
 const PAGE_SIZE = 100;
 
-const fetchMajors = () => axios.get<Lecture[]>('./schedules-majors.json');
-const fetchLiberalArts = () => axios.get<Lecture[]>('./schedules-liberal-arts.json');
+const fetchMajors = () => cachedFetch<Lecture[]>('./schedules-majors.json');
+const fetchLiberalArts = () => cachedFetch<Lecture[]>('./schedules-liberal-arts.json');
 
 const Item = memo(({ onClick, ...lecture }: { onClick: (lecture: Lecture) => void } & Lecture) => {
   const handleClick = () => onClick(lecture);
@@ -106,15 +106,7 @@ const Item = memo(({ onClick, ...lecture }: { onClick: (lecture: Lecture) => voi
 });
 
 // TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
-const fetchAllLectures = async () =>
-  await Promise.all([
-    (console.log('API Call 1', performance.now()), await fetchMajors()),
-    (console.log('API Call 2', performance.now()), await fetchLiberalArts()),
-    (console.log('API Call 3', performance.now()), await fetchMajors()),
-    (console.log('API Call 4', performance.now()), await fetchLiberalArts()),
-    (console.log('API Call 5', performance.now()), await fetchMajors()),
-    (console.log('API Call 6', performance.now()), await fetchLiberalArts()),
-  ]);
+const fetchAllLectures = () => Promise.all([fetchMajors(), fetchLiberalArts()]);
 
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
 const SearchDialog = ({ searchInfo, onClose }: Props) => {
@@ -190,6 +182,8 @@ const SearchDialog = ({ searchInfo, onClose }: Props) => {
   );
 
   useEffect(() => {
+    if (!searchInfo) return;
+
     const start = performance.now();
     console.log('API 호출 시작: ', start);
     fetchAllLectures().then(results => {
@@ -198,7 +192,7 @@ const SearchDialog = ({ searchInfo, onClose }: Props) => {
       console.log('API 호출에 걸린 시간(ms): ', end - start);
       setLectures(results.flatMap(result => result.data));
     });
-  }, []);
+  }, [searchInfo]);
 
   useEffect(() => {
     const $loader = loaderRef.current;
