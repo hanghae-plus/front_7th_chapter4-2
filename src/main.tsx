7b8af64fdd97bdf05fcd760944ab26d16db12336
dasom <ds92ko@gmail.com>
2025-12-26 08:02:07 +0900

    refactor: 중복된 ChakraProvider 제거
    - 중복된 ChakraProvider 제거하여 불필요한 중첩 렌더링 방지
    - App.tsx에서 이미 ChakraProvider를 제공하므로 중복 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
-);
+createRoot(document.getElementById('root')!).render(<App />);
