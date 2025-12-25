7b5d7965a4197b0de8bb4ecbfdfd1e2907ab7b51
천진아 <jncheon@carmerce.co.kr>
2025-12-26 00:47:34 +0900

    refactor: 시간표 블록 드롭시 렌더링 최적화

--- src/ScheduleContext.tsx
+++ src/ScheduleContext.tsx
@@ -1,18 +1,30 @@
-import React, { createContext, PropsWithChildren, useContext, useState } from "react";
+import React, { createContext, PropsWithChildren, useContext, useMemo, useState } from "react";
 import { Schedule } from "./types.ts";
 import dummyScheduleMap from "./dummyScheduleMap.ts";
 
 interface ScheduleContextType {
   schedulesMap: Record<string, Schedule[]>;
+}
+
+interface SetScheduleContextType {
   setSchedulesMap: React.Dispatch<React.SetStateAction<Record<string, Schedule[]>>>;
 }
 
 const ScheduleContext = createContext<ScheduleContextType | undefined>(undefined);
+const SetScheduleContext = createContext<SetScheduleContextType | undefined>(undefined);
 
 export const useScheduleContext = () => {
   const context = useContext(ScheduleContext);
   if (context === undefined) {
-    throw new Error('useSchedule must be used within a ScheduleProvider');
+    throw new Error("useSchedule must be used within a ScheduleProvider");
+  }
+  return context;
+};
+
+export const useSetScheduleContext = () => {
+  const context = useContext(SetScheduleContext);
+  if (context === undefined) {
+    throw new Error("useSetSchedule must be used within a ScheduleProvider");
   }
   return context;
 };
@@ -20,9 +32,12 @@ export const useScheduleContext = () => {
 export const ScheduleProvider = ({ children }: PropsWithChildren) => {
   const [schedulesMap, setSchedulesMap] = useState<Record<string, Schedule[]>>(dummyScheduleMap);
 
+  const scheduleValue = useMemo(() => ({ schedulesMap }), [schedulesMap]);
+  const setScheduleValue = useMemo(() => ({ setSchedulesMap }), [setSchedulesMap]);
+
   return (
-    <ScheduleContext.Provider value={{ schedulesMap, setSchedulesMap }}>
-      {children}
-    </ScheduleContext.Provider>
+    <SetScheduleContext.Provider value={setScheduleValue}>
+      <ScheduleContext.Provider value={scheduleValue}>{children}</ScheduleContext.Provider>
+    </SetScheduleContext.Provider>
   );
 };
--- src/ScheduleTables.tsx
+++ src/ScheduleTables.tsx
@@ -1,12 +1,105 @@
 import { Button, ButtonGroup, Flex, Heading, Stack } from "@chakra-ui/react";
 import ScheduleTable from "./ScheduleTable.tsx";
-import { useScheduleContext } from "./ScheduleContext.tsx";
+import { useScheduleContext, useSetScheduleContext } from "./ScheduleContext.tsx";
 import SearchDialog from "./SearchDialog.tsx";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import ScheduleDndProvider from "./ScheduleDndProvider.tsx";
+import { Schedule } from "./types.ts";
+import { memo } from "react";
+import { useDndContext } from "@dnd-kit/core";
+
+const ScheduleTableBox = memo(
+  ({
+    tableId,
+    schedules,
+    index,
+    setSearchInfo,
+    disabledRemoveButton,
+  }: {
+    tableId: string;
+    schedules: Schedule[];
+    index: number;
+    setSearchInfo: (info: { tableId: string }) => void;
+    disabledRemoveButton: boolean;
+  }) => {
+    const { setSchedulesMap } = useSetScheduleContext();
+
+    const dndContext = useDndContext();
+    const getActiveTableId = () => {
+      const activeId = dndContext.active?.id;
+      if (activeId) {
+        return String(activeId).split(":")[0];
+      }
+      return null;
+    };
+    const activeTableId = getActiveTableId();
+
+    const duplicate = (targetId: string) => {
+      setSchedulesMap((prev: Record<string, Schedule[]>) => ({
+        ...prev,
+        [`schedule-${Date.now()}`]: [...prev[targetId]],
+      }));
+    };
+
+    const remove = (targetId: string) => {
+      setSchedulesMap(prev => {
+        delete prev[targetId];
+        return { ...prev };
+      });
+    };
+
+    const scheduleTimeClick = useCallback(
+      (timeInfo: { day: string; time: number }) => setSearchInfo({ tableId, ...timeInfo }),
+      [tableId],
+    );
+
+    const scheduleDeleteButtonClick = useCallback(
+      ({ day, time }: { day: string; time: number }) =>
+        setSchedulesMap((prev: any) => ({
+          ...prev,
+          [tableId]: prev[tableId].filter(
+            (schedule: Schedule) => schedule.day !== day || !schedule.range.includes(time),
+          ),
+        })),
+      [tableId],
+    );
+
+    return (
+      <Stack key={tableId} width="600px">
+        <Flex justifyContent="space-between" alignItems="center">
+          <Heading as="h3" fontSize="lg">
+            시간표 {index + 1}
+          </Heading>
+          <ButtonGroup size="sm" isAttached>
+            <Button colorScheme="green" onClick={() => setSearchInfo({ tableId })}>
+              시간표 추가
+            </Button>
+            <Button colorScheme="green" mx="1px" onClick={() => duplicate(tableId)}>
+              복제
+            </Button>
+            <Button colorScheme="green" isDisabled={disabledRemoveButton} onClick={() => remove(tableId)}>
+              삭제
+            </Button>
+          </ButtonGroup>
+        </Flex>
+        <ScheduleDndProvider setSchedulesMap={setSchedulesMap}>
+          <ScheduleTable
+            key={`schedule-table-${index}`}
+            isActive={activeTableId === tableId}
+            schedules={schedules}
+            tableId={tableId}
+            onScheduleTimeClick={scheduleTimeClick}
+            onDeleteButtonClick={scheduleDeleteButtonClick}
+          />
+        </ScheduleDndProvider>
+      </Stack>
+    );
+  },
+);
 
 export const ScheduleTables = () => {
-  const { schedulesMap, setSchedulesMap } = useScheduleContext();
+  const { schedulesMap } = useScheduleContext();
+
   const [searchInfo, setSearchInfo] = useState<{
     tableId: string;
     day?: string;
@@ -15,58 +108,21 @@ export const ScheduleTables = () => {
 
   const disabledRemoveButton = Object.keys(schedulesMap).length === 1;
 
-  const duplicate = (targetId: string) => {
-    setSchedulesMap(prev => ({
-      ...prev,
-      [`schedule-${Date.now()}`]: [...prev[targetId]],
-    }));
-  };
-
-  const remove = (targetId: string) => {
-    setSchedulesMap(prev => {
-      delete prev[targetId];
-      return { ...prev };
-    });
-  };
-
   return (
     <>
       <Flex w="full" gap={6} p={6} flexWrap="wrap">
-        {Object.entries(schedulesMap).map(([tableId, schedules], index) => (
-          <Stack key={tableId} width="600px">
-            <Flex justifyContent="space-between" alignItems="center">
-              <Heading as="h3" fontSize="lg">
-                시간표 {index + 1}
-              </Heading>
-              <ButtonGroup size="sm" isAttached>
-                <Button colorScheme="green" onClick={() => setSearchInfo({ tableId })}>
-                  시간표 추가
-                </Button>
-                <Button colorScheme="green" mx="1px" onClick={() => duplicate(tableId)}>
-                  복제
-                </Button>
-                <Button colorScheme="green" isDisabled={disabledRemoveButton} onClick={() => remove(tableId)}>
-                  삭제
-                </Button>
-              </ButtonGroup>
-            </Flex>
-
-            <ScheduleDndProvider>
-              <ScheduleTable
-                key={`schedule-table-${index}`}
-                schedules={schedules}
-                tableId={tableId}
-                onScheduleTimeClick={timeInfo => setSearchInfo({ tableId, ...timeInfo })}
-                onDeleteButtonClick={({ day, time }) =>
-                  setSchedulesMap(prev => ({
-                    ...prev,
-                    [tableId]: prev[tableId].filter(schedule => schedule.day !== day || !schedule.range.includes(time)),
-                  }))
-                }
-              />
-            </ScheduleDndProvider>
-          </Stack>
-        ))}
+        {Object.entries(schedulesMap).map(([tableId, schedules], index) => {
+          return (
+            <ScheduleTableBox
+              key={`schedule-table-${index}`}
+              tableId={tableId}
+              schedules={schedules}
+              index={index}
+              setSearchInfo={setSearchInfo}
+              disabledRemoveButton={disabledRemoveButton}
+            />
+          );
+        })}
       </Flex>
       <SearchDialog searchInfo={searchInfo} onClose={() => setSearchInfo(null)} />
     </>
